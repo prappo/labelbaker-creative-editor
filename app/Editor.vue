73a7d359c5ad94b08e55d0e76b4c334df38d1ed4
Prappo <prappo.prince@gmail.com>
2024-12-03 21:11:06 +0600

    optimize code

--- app/Editor.vue
+++ app/Editor.vue
@@ -1,20 +1,22 @@
 <template>
   <div>
-   
+    <!-- Main editor container -->
     <div class="bg-gray-100 dark:bg-gray-900 dark:text-white text-gray-600 h-screen flex overflow-hidden text-sm">
-     
       <div class="flex-grow overflow-hidden h-full flex flex-col">
-   
+        <!-- Header component -->
         <Header />
-        <div :class="{ hideme: store.activeMenu !== 'designer' }" class="flex-grow flex overflow-x-hidden">
-          <!-- tools -->
 
+        <!-- Main editor area -->
+        <div class="flex-grow flex overflow-x-hidden">
+          <!-- Left sidebar tools panel -->
           <Tools :editor="editor" />
-          <!-- Middle section -->
+
+          <!-- Main canvas viewport -->
           <div id="editor-viewport" class="flex-grow bg-gray-400 dark:bg-gray-900 overflow-y-auto relative">
-            <!-- Settings section -->
+            <!-- Settings overlay -->
             <Settings :editor="editor" />
 
+            <!-- Canvas container -->
             <div @click="performViewportClick" class="sm:p-4 p-4">
               <div class="flex w-full items-center mb-7">
                 <div style="height: 80vh" class="w-full" id="stage-parent-1">
@@ -24,50 +26,30 @@
             </div>
           </div>
 
-          <Properties   :editor="editor" />
-          <ContextMenuVue  :editor="editor" />
+          <!-- Right sidebar properties panel -->
+          <Properties :editor="editor" />
+          
+          <!-- Context menu -->
+          <ContextMenuVue :editor="editor" />
         </div>
-
       </div>
-
     </div>
   </div>
 </template>
-  
+
 <script>
 import { defineAsyncComponent } from "vue";
+import { useToast } from "vue-toastification";
 import { Editor } from "../src/editor";
-// import Tools from "./components/Tools/index.vue";
-const Tools = defineAsyncComponent(() =>
-  import("./components/Tools/index.vue")
-);
 
-// import Header from "./components/Header/Index.vue";
-const Header = defineAsyncComponent(() =>
-  import("./components/Header/Index.vue")
-);
+// Async component imports for better performance
+const Tools = defineAsyncComponent(() => import("./components/Tools/index.vue"));
+const Header = defineAsyncComponent(() => import("./components/Header/Index.vue"));
+const Settings = defineAsyncComponent(() => import("./components/Settings.vue"));
+const Properties = defineAsyncComponent(() => import("./components/Properties/index.vue"));
+const ContextMenuVue = defineAsyncComponent(() => import("./components/ContextMenu.vue"));
 
-// import Sidebar from "./components/Sidebar/Index.vue";
-const Sidebar = defineAsyncComponent(() =>
-  import("./components/Sidebar/Index.vue")
-);
-
-// import Settings from "./components/Settings.vue";
-const Settings = defineAsyncComponent(() =>
-  import("./components/Settings.vue")
-);
-
-// import Properties from "./components/Properties/index.vue";
-const Properties = defineAsyncComponent(() =>
-  import("./components/Properties/index.vue")
-);
-
-// import ContextMenuVue from "./components/ContextMenu.vue";
-const ContextMenuVue = defineAsyncComponent(() =>
-  import("./components/ContextMenu.vue")
-);
-
-
+// Store imports
 import { store } from "./store/menu";
 import { AppSettings } from "./store/app-settings";
 import { templates } from "./store/templates";
@@ -78,49 +60,30 @@ import { responsive } from "./store/responsive";
 import { userData } from "./store/user";
 import { TempData } from "./store/temp";
 
-
-
-
-
-import { saveUserData, getUserData, saveAsTemplate } from "./libs/helper";
-
-
-
-
-
-import { getCurrentInstance } from "vue";
-import { useToast } from "vue-toastification";
-import { enc, useIt } from "./libs/helper";
-import { getTemplateById } from "./libs/template"
+// Helper functions
+import { saveUserData, saveAsTemplate } from "./libs/helper";
 
 export default {
+  name: 'Editor',
   components: {
     Tools,
     Header,
-    Sidebar,
     Settings,
     Properties,
-
-
- 
     ContextMenuVue,
-
   },
 
   data() {
     return {
-      name: "prappo",
       editor: null,
       store,
       userData,
       TempData,
-      isExpired: false,
       stats,
       templates,
       AppSettings,
       canvasContainer,
       EditorSettings,
-      isLoogedIn: false,
       vcount: 0,
       hcount: 0,
       responsive,
@@ -128,208 +91,134 @@ export default {
   },
 
   methods: {
-    changeRoute(route) {
-      const routeList = {
-        "#/dashboard": {
-          activeMenu: "home",
-          activeTool: "myTemplates",
-        },
-        "#/templates": {
-          activeMenu: "home",
-          activeTool: "myTemplates",
-        },
-        "#/new": {
-          activeMenu: "home",
-          activeTool: "newProject",
-        },
-        "#/editor": {
-          activeMenu: "designer",
-          activeTool: "shape",
-        },
-        "#/maker": {
-          activeMenu: "maker",
-          activeTool: "layoutMaker",
-        },
-        "#/license": {
-          activeMenu: "license",
-          activeTool: "licenseEntry",
-        },
-        "#/banners": {
-          activeMenu: "banners",
-          activeTool: "list",
-        },
-        "#/popups": {
-          activeMenu: "popups",
-          activeTool: "list",
-        },
-        "#/labels": {
-          activeMenu: "labels",
-          activeTool: "list",
-        },
-        "#/apps": {
-          activeMenu: "apps",
-          activeTool: "list",
-        },
-        "#/thumbnails": {
-          activeMenu: "thumbnails",
-          activeTool: "list",
-        },
-        "#/products": {
-          activeMenu: "products",
-          activeTool: "list",
-        },
-        "#/email": {
-          activeMenu: "email",
-          activeTool: "list",
-        },
-        "#/generators": {
-          activeMenu: "generators",
-          activeTool: "list",
-        },
-        "#/forms": {
-          activeMenu: "forms",
-          activeTool: "list",
-        },
-      };
-
-      const templateID = (url) => {
-        // Define the regex pattern for matching "#/editor/{template_id}"
-        const pattern = /^#\/editor\/(\w+)$/;
-
-        // Test if the URL matches the pattern
-        const match = url.match(pattern);
-
-        // If there is a match, return the captured template_id, otherwise return null
-        return match ? match[1] : null;
-      };
-
-      const isEditorUrl = (url) => {
-
-        // Define the regex pattern for matching "#/editor/{template_id}"
-        const pattern = /^#\/editor\/\w+$/;
-
-        // Test if the URL matches the pattern
-        return pattern.test(url);
-      };
-
-      if (routeList[route] || isEditorUrl(route)) {
-        if (isEditorUrl(route)) {
-
-          this.store.activeMenu = routeList['#/editor'].activeMenu;
-          this.store.activeTool = routeList['#/editor'].activeTool;
-
-          // Go to editor page to edit template or image
-
-          setTimeout(() => {
-            stats.isBusy = true;
-            const id = templateID(route); // get template id or post data
-
-            if (id) {
-              // if editor is bening called to edit post thumbnail
-              if (id.includes('post')) {
-                let postData = id.split('_')
-                let postId = postData[1]
-
-
-                wp.apiRequest({
-                  path: "labelbaker/v1/featured-image?post_id=" + postId
-                }).done(data => {
-
-                  TempData.editing.id = postId;
-                  TempData.templateType = ['']
-                  TempData.editing.name = "##Featured Image##"
-                  useIt(data, 'image');
-                  stats.isBusy = false;
-                }).fail(err => {
-                  TempData.editing.id = postId;
-                  TempData.editing.name = "##Featured Image##"
-
-                  stats.isBusy = false;
-                })
-              } else {
-                // otherwise find template and import on editor to edit
-                getTemplateById(id, this.serverEndpoint).then(data => {
-                  useIt(data);
-                  stats.isBusy = false;
-                }).catch(e => {
-                  // alert('Template not found')
-                  // location.href = document.referrer
-                  stats.isBusy = false;
-
-                  return;
-
-                })
-              }
-
-
-            }
-
-            this.canvasContainer.editor.fitIntoContainer();
-          }, 500);
-        } else {
-          let isCreated = false;
-          if (route === '#/editor' && this.TempData.editing.name && this.TempData.templateType && !isCreated) {
-         
-              let templateID = Date.now().toString();
-              saveAsTemplate(
-                templateID,
-                this.TempData.editing.name,
-                this.userData.details.email,
-                this.canvasContainer,
-                this.serverEndpoint,
-                this.appMode,
-                this.TempData.templateType
-              ).then((e) => {
-                getTemplateById(templateID, this.serverEndpoint).then(data => {
-                  useIt(data);
-                  stats.isBusy = false;
-                }).catch(e => {
-                  // alert('Template not found')
-                  // location.href = document.referrer
-                  stats.isBusy = false;
-                  isCreated = true;
-                  return;
-
-                })
-              });
-        
-          }
-          this.store.activeMenu = routeList[route].activeMenu;
-          this.store.activeTool = routeList[route].activeTool;
-        }
-
-        // if (route === "#/editor") {
-        //   setTimeout(() => {
-        //     this.canvasContainer.editor.fitIntoContainer();
-        //   }, 500);
-        // }
-      }
-    },
+    // Handle viewport click
     performViewportClick() {
       responsive.hideProperties = true;
       responsive.hideSidebar = true;
     },
-    rulerControl: function () {
-      if (this.canvasContainer.enableRuler) {
-        this.canvasContainer.editor.enableRuler = true;
-      } else {
-        this.canvasContainer.editor.enableRuler = false;
-      }
+
+    // Toggle ruler visibility
+    rulerControl() {
+      this.canvasContainer.editor.enableRuler = this.canvasContainer.enableRuler;
       this.canvasContainer.editor.ruler.addRuler();
     },
 
-    gridControl: function () {
-      if (this.canvasContainer.enableGrid) {
-        this.canvasContainer.editor.enableGrid = true;
-      } else {
-        this.canvasContainer.editor.enableGrid = false;
-      }
+    // Toggle grid visibility
+    gridControl() {
+      this.canvasContainer.editor.enableGrid = this.canvasContainer.enableGrid;
       this.canvasContainer.editor.grid.addGrid();
     },
-  },
-  mounted() {
-   
 
+    // Initialize keyboard shortcuts
+    initKeyboardShortcuts() {
+      this._keyListener = this.handleKeyboardShortcuts.bind(this);
+      document.addEventListener("keydown", this._keyListener);
+    },
+
+    // Handle keyboard shortcuts
+    handleKeyboardShortcuts(e) {
+      // Save shortcut (Ctrl/Cmd + S)
+      if (e.key === "s" && (e.ctrlKey || e.metaKey)) {
+        e.preventDefault();
+        this.handleSave();
+      }
+
+      // Undo shortcut (Ctrl/Cmd + Z)
+      if (e.key === "z" && (e.ctrlKey || e.metaKey)) {
+        e.preventDefault();
+        this.editor.undo();
+      }
+
+      // New label shortcut (Ctrl/Cmd + L)
+      if (e.key === "l" && (e.ctrlKey || e.metaKey)) {
+        e.preventDefault();
+        this.stats.showNewModal = true;
+      }
+
+      // Handle arrow key navigation for selected objects
+      switch (e.key) {
+        case 'ArrowUp':
+          if (this.canvasContainer.editor.selected) {
+            this.canvasContainer.editor.selected.y(
+              this.canvasContainer.editor.selected.y() - 1
+            );
+          }
+          break;
+        case 'ArrowDown':
+          if (this.canvasContainer.editor.selected) {
+            this.canvasContainer.editor.selected.y(
+              this.canvasContainer.editor.selected.y() + 1
+            );
+          }
+          break;
+        case 'ArrowLeft':
+          if (this.canvasContainer.editor.selected) {
+            this.canvasContainer.editor.selected.x(
+              this.canvasContainer.editor.selected.x() - 1
+            );
+          }
+          break;
+        case 'ArrowRight':
+          if (this.canvasContainer.editor.selected) {
+            this.canvasContainer.editor.selected.x(
+              this.canvasContainer.editor.selected.x() + 1
+            );
+          }
+          break;
+      }
+    },
+
+    // Handle save functionality
+    handleSave() {
+      if (this.TempData.editing.id) {
+        saveAsTemplate(
+          this.TempData.editing.id,
+          this.TempData.editing.name,
+          this.userData.details.email,
+          this.canvasContainer,
+          this.serverEndpoint,
+          this.appMode
+        ).then(() => {
+          this.templates.templates = null;
+        });
+        return;
+      }
+
+      let container = {};
+      for (let index in this.canvasContainer) {
+        if (index != "editor") {
+          container[index] = this.canvasContainer[index];
+        }
+      }
+
+      this.stats.isBusy = true;
+      saveUserData(this.serverEndpoint, this.editor, container, this.userData)
+        .then(() => {
+          this.stats.isBusy = false;
+          const toast = useToast();
+          toast.success("Saved !", {
+            position: "bottom-center",
+            timeout: 2000,
+            closeOnClick: true,
+            pauseOnFocusLoss: true,
+            pauseOnHover: true,
+            draggable: true,
+            draggablePercent: 0.6,
+            showCloseButtonOnHover: false,
+            hideProgressBar: true,
+            closeButton: "button",
+            icon: true,
+            rtl: false,
+          });
+        })
+        .catch(() => {
+          alert("Unable to save data");
+        });
+    }
+  },
+
+  mounted() {
+    // Initialize editor
     const config = {
       width: parseInt(this.canvasContainer.cellWidth),
       height: parseInt(this.canvasContainer.cellHeight),
@@ -339,129 +228,32 @@ export default {
       pageWidth: parseInt(this.canvasContainer.pageWidth),
     };
 
+    // Create editor instance
     const myEditor = new Editor(config);
     this.canvasContainer.editor = myEditor;
+    this.editor = myEditor;
+    
+    // Development mode exposure
     if (import.meta.env.MODE === "development") {
       window.editor = myEditor;
     }
 
-    this.editor = myEditor;
+    // Fit editor to container
     myEditor.fitIntoContainer();
-    window.addEventListener("resize", function () {
-      myEditor.fitIntoContainer();
-    });
+    window.addEventListener("resize", () => myEditor.fitIntoContainer());
 
-    const app = getCurrentInstance();
-  
-   
+    // Initialize keyboard shortcuts
+    this.initKeyboardShortcuts();
 
+    // Load semitic setting if exists
     if (localStorage.getItem("semitic")) {
       this.canvasContainer.semitic = localStorage.getItem("semitic");
     }
 
-    this._keyListener = function (e) {
-      if (e.key === "s" && (e.ctrlKey || e.metaKey)) {
-        e.preventDefault(); // present "Save Page" from getting triggered.
-
-        if (this.TempData.editing.id) {
-          saveAsTemplate(
-            this.TempData.editing.id,
-            this.TempData.editing.name,
-            this.userData.details.email,
-            this.canvasContainer,
-            this.serverEndpoint,
-            this.appMode
-          ).then((e) => {
-            this.templates.templates = null;
-          });
-          return;
-        }
-        let container = {};
-
-        for (let index in this.canvasContainer) {
-          if (index != "editor") {
-            container[index] = this.canvasContainer[index];
-          }
-        }
-
-        this.stats.isBusy = true;
-
-        saveUserData(this.serverEndpoint, this.editor, container, user)
-          .then((e) => {
-            this.stats.isBusy = false;
-            const toast = useToast();
-            toast.success("Saved !", {
-              position: "bottom-center",
-              timeout: 2000,
-              closeOnClick: true,
-              pauseOnFocusLoss: true,
-              pauseOnHover: true,
-              draggable: true,
-              draggablePercent: 0.6,
-              showCloseButtonOnHover: false,
-              hideProgressBar: true,
-              closeButton: "button",
-              icon: true,
-              rtl: false,
-            });
-          })
-          .catch((e) => {
-            alert("Unable to save data");
-          });
-      }
-
-      if (e.key === "z" && (e.ctrlKey || e.metaKey)) {
-        e.preventDefault(); // present "Save Page" from getting triggered.
-        this.editor.undo();
-      }
-
-      // New label
-      if (e.key === "l" && (e.ctrlKey || e.metaKey)) {
-        e.preventDefault(); // present "Save Page" from getting triggered.
-        this.stats.showNewModal = true;
-      }
-
-
-
-      // object move
-
-      switch (e.key) {
-        case 'ArrowUp':
-          canvasContainer.editor.selected.y(
-            canvasContainer.editor.selected.y() - 1
-          )
-          break;
-        case 'ArrowDown':
-          canvasContainer.editor.selected.y(
-            canvasContainer.editor.selected.y() + 1
-          )
-          break;
-        case 'ArrowLeft':
-          canvasContainer.editor.selected.x(
-            canvasContainer.editor.selected.x() - 1
-          )
-          break;
-        case 'ArrowRight':
-          canvasContainer.editor.selected.x(
-            canvasContainer.editor.selected.x() + 1
-          )
-          break;
-        default:
-          break;
-      }
-    };
-
-    document.addEventListener("keydown", this._keyListener.bind(this));
-
+    // Update Konva content class
     document
       .querySelector("div.konvajs-content")
       .classList.replace("konvajs-content", "label-baker-content");
-
-    this.changeRoute(window.location.hash);
-    window.addEventListener("hashchange", () => {
-      // this.currentPath = window.location.hash;
-      this.changeRoute(window.location.hash);
-    });
   },
 };
 </script>
--- app/main.js
+++ app/main.js
@@ -1,103 +1,100 @@
-import { createApp , defineAsyncComponent } from "vue";
-import Toast from "vue-toastification";
-import { useToast } from "vue-toastification";
+// Import core Vue dependencies
+import { createApp, defineAsyncComponent } from "vue";
 
-import { createI18n } from "vue-i18n";
-import lang from "./libs/lang";
+// Import third-party plugins
+import Toast, { useToast } from "vue-toastification";
 import "vue-toastification/dist/index.css";
+import { createI18n } from "vue-i18n";
 import { vfmPlugin } from "vue-final-modal";
-import "../style.css";
-// import App from "./App.vue";
-const App = defineAsyncComponent(()=>import('./App.vue'))
-import config from "./libs/config";
 import FontFaceObserver from "fontfaceobserver";
 
-var font = new FontFaceObserver("Libre Barcode 39");
+// Import local dependencies
+import lang from "./libs/lang";
+import config from "./libs/config";
+import "../style.css";
+
+// Load App component asynchronously for better performance
+const App = defineAsyncComponent(() => import('./App.vue'));
+
+// Initialize toast notification
 const toast = useToast();
 
-font
+// Load custom barcode font
+const barcodeFont = new FontFaceObserver("Libre Barcode 39");
+barcodeFont
   .load()
-  .then(function () { })
-  .catch(function () {
+  .catch(() => {
     console.log("unable to load Libre Barcode 39");
   });
 
-const messages = lang;
+// Initialize i18n (internationalization)
+const getLocal = () => localStorage.getItem("lang") || "en";
 
-const getLocal = () => {
-  if (localStorage.getItem("lang")) {
-    return localStorage.getItem("lang");
-  }
-
-  return "en";
-};
 const i18n = createI18n({
-  locale: getLocal(), // set locale
-  fallbackLocale: "en", // set fallback locale
-  messages, // set locale messages
+  locale: getLocal(),
+  fallbackLocale: "en",
+  messages: lang,
 });
+
+// Initialize Vue application
 const selector = "label-baker";
-const docSelector = document.getElementById(selector);
+const app = createApp(App);
 
-const app = createApp(App).use(Toast);
-
+// Register plugins
+app.use(Toast);
 app.use(i18n);
 app.use(vfmPlugin);
 
+// Configure global error handling
 app.config.warnHandler = function (msg, vm, trace) {
-  // `trace` is the component hierarchy trace
+  // Handle Vue warnings here
 };
 
-// app.config.compilerOptions.isCustomElement = tag => tag.startsWith('svg:');
-
-app.config.globalProperties.toast = toast;
-app.config.globalProperties.apiEndpoint = 'https://cdn.jsdelivr.net/gh/prappo/label-template-api@1.1.1/public';
-// app.config.globalProperties.apiEndpoint = '';
-// app.config.globalProperties.apiEndpoint =
-//   "https://raw.githubusercontent.com/prappo/label-template-api/main/public";
-
-app.config.globalProperties.serverEndpoint = window.labelbakerApi;
-// app.config.globalProperties.apiURL = "https://api.labelbaker.com";
-app.config.globalProperties.apiURL = "";
-// app.config.globalProperties.shareURL = 'https://share.labelbaker.com';
-app.config.globalProperties.shareURL = '';
-app.config.globalProperties.isWooExists = false;
-// Application mode
-app.config.globalProperties.appMode = 'noAuth';
-
-// app.config.globalProperties.appMode = 'auth';
-
-app.config.globalProperties.isPro = (isProRequired) => {
-  // Check if pro plugin is available and if the function requires the pro version
+// Configure global properties
+const globalProps = {
+  toast,
+  apiEndpoint: 'https://cdn.jsdelivr.net/gh/prappo/label-template-api@1.1.1/public',
+  serverEndpoint: window.labelbakerApi,
+  apiURL: "",
+  shareURL: '',
+  isWooExists: false,
+  appMode: 'noAuth',
+  defaultAudioUrl: '/music.mp3',
   
-  return false;
-}
-// app.config.globalProperties.exportURL = 'http://labelbaker.local/wp-json/labelbaker/v1/image-upload';
+  // Check if pro features are available
+  isPro: (isProRequired) => false
+};
 
-// Change the required configuration
-// If current environment is development 
+// Apply global properties
+Object.entries(globalProps).forEach(([key, value]) => {
+  app.config.globalProperties[key] = value;
+});
 
+// Development mode configuration
 if (import.meta.env.MODE === "development") {
   console.log("Development mode");
-  // app.config.globalProperties.serverEndpoint = "http://labelbaker.local/wp-json/labelbaker/v1";
-  // app.config.globalProperties.serverEndpoint = "http://localhost:8080";
-  // app.config.globalProperties.apiURL = "http://localhost:1337";
+  // Add development-specific configurations here
 }
 
-app.config.globalProperties.defaultAudioUrl = '/music.mp3'
-
-// if using in wordpress
+// Application integration
 if (typeof labelbaker !== 'undefined') {
-  app.config.globalProperties.serverEndpoint = labelbaker.serverEndpoint;
-  app.config.globalProperties.exportURL = labelbaker.serverEndpoint + '/image-upload'
-  app.config.globalProperties.shareURL = labelbaker.shareURL;
-  app.config.globalProperties.emailSendingApi = labelbaker.emailSendingApi;
-  app.config.globalProperties.userEmail = labelbaker.userEmail;
-  app.config.globalProperties.defaultVideoUrl = labelbaker.defaultVideoUrl;
-  app.config.globalProperties.defaultAudioUrl = labelbaker.defaultAudioUrl;
-  app.config.globalProperties.licenseEndpoint = labelbaker.licenseEndpoint;
-  app.config.globalProperties.isWooExists = labelbaker.isWooExists;
+  const appProps = {
+    serverEndpoint: labelbaker.serverEndpoint,
+    exportURL: `${labelbaker.serverEndpoint}/image-upload`,
+    shareURL: labelbaker.shareURL,
+    emailSendingApi: labelbaker.emailSendingApi,
+    userEmail: labelbaker.userEmail,
+    defaultVideoUrl: labelbaker.defaultVideoUrl,
+    defaultAudioUrl: labelbaker.defaultAudioUrl,
+    licenseEndpoint: labelbaker.licenseEndpoint,
+    isWooExists: labelbaker.isWooExists
+  };
+
+  // Apply application specific properties
+  Object.entries(appProps).forEach(([key, value]) => {
+    app.config.globalProperties[key] = value;
+  });
 }
 
-
+// Mount the application
 app.mount(`#${selector}`);
